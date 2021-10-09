941ab3ed719048f4306e0fbefb82b228012d11a6
JpbmOfficial <31043326+JpbmOfficial@users.noreply.github.com>
2021-10-09 17:12:30 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-      if (message.author.id !== ('390960333314850817')) return message.channel.send("You aren't my owner, sorry.");
-      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You must have Manage Messages permission to do that.");
+      if (message.author.id !== ('390960333314850817')) return message.channel.send("<@${message.author.id}>, you aren't my owner, sorry.");
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
