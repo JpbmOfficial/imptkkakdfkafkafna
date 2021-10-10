48713ccabcb08c60f1e3032551e2367fd6fdc5e2
JpbmOfficial <31043326+JpbmOfficial@users.noreply.github.com>
2021-10-10 21:19:08 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     let inviteembed = new Discord.RichEmbed()
     .setDescription("Invite Link")
     .setColor("RANDOM")
-    .addField("Invite Link",  "https://discordapp.com/oauth2/authorize?client_id=429770671833219092&scope=bot&permissions=0")
+    .addField("Invite Link",  "https://discordapp.com/oauth2/authorize?client_id=429770671833219092&scope=bot&permissions=8")
   .setFooter("impt");
 
     message.channel.send(inviteembed);
