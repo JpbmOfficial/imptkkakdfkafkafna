bc0aa1f5dbfff479338b24d39e8aa5568aab95e8
JpbmOfficial <31043326+JpbmOfficial@users.noreply.github.com>
2021-10-11 22:22:56 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,17 +1,17 @@
 const Discord = require("discord.js");
-const authorized = [
-    "390960333314850817",
-    "813891156348764171"
+//const authorized = [
+   // "390960333314850817",
+   // "813891156348764171"
     
-];
+//];
 
 //if (!authorized.includes(message.author.id)){
  //  return message.channel.send("you do not have permissions");
 //}
 
 module.exports.run = async (bot, message, args) => {
-      if (!authorized.includes(message.author.id)){ return message.channel.send(`<@${message.author.id}>, get fucked retarded.`)
-                                                  }
+    //  if (!authorized.includes(message.author.id)){ return message.channel.send(`<@${message.author.id}>, get fucked retarded.`)
+                                                 // }
       // if (message.author.id !== ('390960333314850817')) return message.channel.send(`<@${message.author.id}>, get fucked retarded.`)
       message.delete().catch();
       const sayMessage = args.join(" ");
