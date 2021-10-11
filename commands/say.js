b17ca787816925f7087af993d8eff8e16d62daed
JpbmOfficial <31043326+JpbmOfficial@users.noreply.github.com>
2021-10-11 21:10:41 +0100

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-      // if (message.author.id !== ('390960333314850817')) return message.channel.send(`<@${message.author.id}>, get fucked retarded.`)
+      if (message.author.id !== ('390960333314850817')) return message.channel.send(`<@${message.author.id}>, get fucked retarded.`)
+      if (message.author.id !== ('813891156348764171')) return message.channel.send(`<@${message.author.id}>, get fucked retarded.`)
       message.delete().catch();
       const sayMessage = args.join(" ");
       message.delete().catch();
