14a4cba4c40705d81129c5ed7b57faa8a2ad604c
JpbmOfficial <31043326+JpbmOfficial@users.noreply.github.com>
2021-10-11 19:12:37 +0100

    Update figlet.js

--- commands/figlet.js
+++ commands/figlet.js
@@ -2,9 +2,9 @@ var figlet = require('figlet');
 
 exports.run = (bot, message, args) => {
   
-  var maxLen = 14 // You can modify the max characters here
+  var maxLen = 30 // You can modify the max characters here
   
-  if(args.join(' ').length > maxLen) return message.channel.send('Only 14 characters admitted!') 
+  if(args.join(' ').length > maxLen) return message.channel.send('Only 30 characters admitted!') 
   
   if(!args[0]) return message.channel.send('Please specify a test to asciify!');
   
